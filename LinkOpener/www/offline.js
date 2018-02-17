b0cdb34670eee29bba6fc30d8b1fbe74976fc53c
zznidar <zznidar@users.noreply.github.com>
2018-02-17 14:11:59 +0100

    Upload LinkOpener exported files for testing purposes

---
+++ LinkOpener/www/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1509914928,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"linkopener_linki.txt",
+		"linki_slovenia.txt",
+		"linki_slovenia_opombe_2017-11-05_21-21.txt",
+		"linki_slovenia2.txt",
+		"linki_slovenia_opombe_2017-11-05_21-22.txt"
+	]
+}
